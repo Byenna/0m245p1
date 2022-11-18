a2701e3ce894ff75746aa5107f9d78af2c0c9267
Juraia <byenna21@gmail.com>
2022-11-18 12:08:10 +0100

    registrate icons and fonts

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,14 @@
+module.exports = {
+    karoe: {
+        area: 'frontend',
+        name: 'MountainiT/karoe',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    }
+};
